f1e3eebe2e0945cbac5f148cb1a525b0db4a41fd
phananh1310 <phananh13102001@gmail.com>
2021-09-28 14:45:48 +0700

    Create Assignment1.c
    Simple program counting number of word and number in a file.

---
+++ Week 1/Assignment1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+  if (argc <= 1) {
+    printf("scanner: no input file.\n");
+    return -1;
+  }
+
+  if (scan(argv[1]) == IO_ERROR) {
+    printf("Can\'t read input file!\n");
+    return -1;
+  }
+    
+  return 0;
+}
